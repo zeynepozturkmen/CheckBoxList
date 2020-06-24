8e47b27c9d6a8ff627eb9e43feb92ac7082b5dcd
zeynep öztürkmen <zynpoztrkmn@hotmail.com>
2020-06-24 17:35:50 +0300

    Add project files.

---
+++ CheckBoxList/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using CheckBoxList.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckBoxList.Controllers
+{
+    public class HomeController : Controller
+    {
+        CheckBoxListEntities db = new CheckBoxListEntities();
+        public ActionResult Index()
+        {
+            var model = new List<PersonList>();
+
+            var list = db.People.ToList();
+            foreach (var item in list)
+            {
+                var md = new PersonList();
+                md.Id = item.Id;
+                md.FullName = item.FullName;
+                md.Email = item.Email;
+                md.Salary = item.Salary;
+
+                model.Add(md);
+
+            }
+            return View(model);
+        }
+
+        public ActionResult UpdatePerson(IEnumerable<PersonList> PersonList)
+        {
+            foreach (var item in PersonList)
+            {
+                var person = db.People.Where(x => x.Id == item.Id).FirstOrDefault();
+                if (person!=null)
+                {
+                    person.Salary = person.Salary*2;
+
+                    db.SaveChanges();
+                }
+            }
+
+            return Json(true, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ CheckBoxList/Models/PersonList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckBoxList.Models
+{
+    public class PersonList
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public decimal? Salary { get; set; }
+    }
+}
